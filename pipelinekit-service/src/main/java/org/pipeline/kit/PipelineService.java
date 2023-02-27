83c65a62c3ecbdae4378b543961195209d59e049
RolandSall <roland.salloum00@outlook.com>
2023-02-27 21:50:35 +0200

    fix: pipeline service app

--- pipelinekit-service/src/main/java/org/pipeline/kit/PipelineService.java
+++ pipelinekit-service/src/main/java/org/pipeline/kit/PipelineService.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppLauncher {
+public class PipelineService {
     public static void main(String[] args) {
-        SpringApplication.run(AppLauncher.class, args);
+        SpringApplication.run(PipelineService.class, args);
     }
 }
